4b39a02216aa6ef62e0fd8a654245ec563fc2f40
Luke <lucasthebigreddog@yahoo.com>
2021-01-31 03:15:57 -0500

    add hof feature

--- hunt_bot.py
+++ hunt_bot.py
@@ -4,22 +4,22 @@ from discord import client
 from discord import member
 from discord.ext import commands
 from discord.ext.commands import Bot
+from discord.utils import get
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
 client = commands.Bot(command_prefix ='.')
 
+code_au = []
+hof_guild_channels = { 734158340799725568: 805317843238912021, 515215609252806694: 805344270516355092 }
+
 @client.event
 async def on_ready():
     print('Bot is ready')
-    
 
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined a server.')
-    
-code_au = []
-
 
 @client.command()
 async def setcode(ctx, code):
@@ -29,7 +29,7 @@ async def setcode(ctx, code):
 
 @client.command()
 async def code(ctx):
-    await ctx.send(code_au)
+    await ctx.send(code_au[0])
 
 @client.command()
 async def avatar(ctx, *,  avamember : discord.Member=None):
@@ -293,11 +293,37 @@ async def colby2(ctx, ):
 async def kenny2(ctx, ):
     await ctx.send('basically you just take the ting and you just whack her in the face. one time. assert your dominance')
 
+@client.event
+async def on_raw_reaction_add(payload):
+    print("-------------------------------------------------")
+    print(payload)
+    print(payload.guild_id)
+    print("-------------------------------------------------")
+    
+    if payload.emoji.name == '⭐':
+
+        guild_id = payload.guild_id
+        channel_id = hof_guild_channels[guild_id]
+        hof_channel = client.get_channel(channel_id)
+        message_channel = client.get_channel(payload.channel_id)
+        message = await message_channel.fetch_message(payload.message_id)
+
+        reaction = get(message.reactions, emoji=payload.emoji.name)
+
+        if reaction and reaction.count > 5 and hof_channel is not None:
+            hof_messages = await hof_channel.history(limit=10).flatten()
+
+            for hof_message in hof_messages:
+                if (hof_message.embeds and str(message.id) in hof_message.embeds[0].description):
+                    message_to_edit = await hof_channel.fetch_message(hof_message.id)
+                    await message_to_edit.edit(content = '⭐ ' + str(reaction.count))
+                    return
+                    
+            embed = discord.Embed(title=message.author.name, description=link_generator('Jump to message', message.jump_url) + '\n' + message.content)
+            await hof_channel.send('⭐ ' + str(reaction.count), embed = embed)
 
 
-
-
-
-
-
+def link_generator(text, url):
+    return '[' + text + '](' + url + ')'
+    
 client.run(TOKEN)
